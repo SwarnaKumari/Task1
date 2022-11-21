0daad2f34dae3a07e7068ff7dcb0dc459a618981
SwarnaKumari <50829003+SwarnaKumari@users.noreply.github.com>
2022-11-21 16:37:48 +0530

    Update String.kt

--- Array/String.kt
+++ Array/String.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>){
     var name="swarna"
+    var name2="swarnakumari"
     println(name)
     println(name[0])
     println(name[1])
@@ -9,4 +10,15 @@ fun main(args: Array<String>){
     val ch=charArrayOf('h','e','l','l','o')
     val st=String(ch)
     println(st)
+    println(name.get(4))
+    println(name.subSequence(2,5))
+    println(name.compareTo(name2))
+    //escaped string
+    var str="my \n name is \n swarna"
+    println(str)
+    //raw string
+    var str2="""My 
+        |is
+        |chandini""".trimMargin()
+    println(str2)
 }
