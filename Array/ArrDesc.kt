61729a9d5ecb7102410da2591453a411492ab290
SwarnaKumari <50829003+SwarnaKumari@users.noreply.github.com>
2022-11-21 16:03:45 +0530

    Add files via upload

---
+++ Array/ArrDesc.kt
@@ -0,0 +1,22 @@
+package ArrayTask
+import java.util.*
+fun main(args: Array<String>)
+{
+    var scan=Scanner(System.`in`)
+    println("Enter size of array:")
+    var mysize=scan.nextInt()
+    var numbers=Array(mysize,{0})
+    println("Enter elements into array:")
+    for(i in 0..(mysize-1)){
+        numbers.set(i,scan.nextInt())
+    }
+    println("Elements before ascending order:")
+    for(i in 0..mysize-1) {
+        println("${numbers[i]}")
+    }
+    println("After ascending ")
+    numbers.sortDescending()
+    for(i in 0..(mysize-1)){
+        println("${numbers[i]}")
+    }
+}
