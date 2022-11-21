b647d267293cd60b8e3d4f1b714b08af0331c868
SwarnaKumari <50829003+SwarnaKumari@users.noreply.github.com>
2022-11-21 16:11:45 +0530

    Add files via upload

---
+++ Array/String.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    var name="swarna"
+    println(name)
+    println(name[0])
+    println(name[1])
+    println("length of string:${name.length}")
+    println("string indices:${name.indices}")
+    println("Last index of string:${name.lastIndex}")
+    val ch=charArrayOf('h','e','l','l','o')
+    val st=String(ch)
+    println(st)
+}
